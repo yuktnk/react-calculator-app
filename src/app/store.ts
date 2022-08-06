894eb232e100e49b1a3364d43e09f105244d002d
yuktnk <yuki0wl@gmail.com>
2022-08-06 21:40:12 +0900

    RTKからcounterを削除

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
 import calculatorSlice from '../features/calculator/calculatorSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    calculator: calculatorSlice
-  }
+    calculator: calculatorSlice,
+  },
 })
 
 export type AppDispatch = typeof store.dispatch
