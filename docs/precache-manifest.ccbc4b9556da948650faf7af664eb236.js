e515dadf85e56a64b896a416d6dbc835679565dd
yuktnk <yuki0wl@gmail.com>
2022-08-06 22:38:32 +0900

    deploy test

---
+++ docs/precache-manifest.ccbc4b9556da948650faf7af664eb236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "93711214a80e14021fa1",
+    "url": "/react-calculator-app/static/css/main.ff460775.chunk.css"
+  },
+  {
+    "revision": "93711214a80e14021fa1",
+    "url": "/react-calculator-app/static/js/main.93711214.chunk.js"
+  },
+  {
+    "revision": "90ecf6b5710b2396cef7",
+    "url": "/react-calculator-app/static/js/1.90ecf6b5.chunk.js"
+  },
+  {
+    "revision": "0345afa4142b119b7f59",
+    "url": "/react-calculator-app/static/js/2.0345afa4.chunk.js"
+  },
+  {
+    "revision": "6d8e7b92f62eda0bbc78",
+    "url": "/react-calculator-app/static/js/runtime~main.6d8e7b92.js"
+  },
+  {
+    "revision": "7e945915063cebc61a95421d49ad92f4",
+    "url": "/react-calculator-app/index.html"
+  }
+];
